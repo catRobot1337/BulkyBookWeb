9a5a0f3d5147e567c4cc77426f558cecb4cb46d9
nice1373 <nice1373@outlook.com>
2022-02-04 00:58:19 -0500

    1. created category controller 1a. created index view from category controller 2. created category model 3. created ApplicationDbContext as part of Entity Framework process in creating script to create to db 4. added db connection string to appsettings.json server/db/using default trusted connection true 5. installed entity framework core 5a. installed efc sql server 5b. installed efc tools 6. added DbContext and ApplicationDbContext service to connect with our Default Connection sql db server from (4)

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models;
+
+public class Category
+{
+    [Key] //key annotation tells entity framework code to ensure Id is a PK and should also be an identity column when it creates the script, does all the talking and configuration by itself
+    public int Id { get; set; }
+    [Required] //req annotation ensures that there name must not be null before EF creates the script
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+    public DateTime CreatedDateTime { get; set; } = DateTime.Now; //default value automatically assigned to datetime when we make an object of this class
+}
+
+
