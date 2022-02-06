0eded1797b6b31db330a962ecc8df5a7b2c86600
nice1373 <nice1373@outlook.com>
2022-02-05 22:49:17 -0500

    Progress ... review and add notes later

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -2,29 +2,47 @@
 using BulkyBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BulkyBookWeb.Controllers
+namespace BulkyBookWeb.Controllers;
+
+public class CategoryController : Controller
 {
-    public class CategoryController : Controller
+    private readonly ApplicationDbContext _db; //private class _db using ApplicationDbContext
+
+    public CategoryController(ApplicationDbContext db)
     {
-        private readonly ApplicationDbContext _db; //private class _db using ApplicationDbContext
-
-        public CategoryController(ApplicationDbContext db)
-        {
-            _db = db;
-        }
-
-        public IActionResult Index()
-        {
-            IEnumerable<Category> objCategoryList = _db.Categories; //added to retrieve all categories from _db database Categories table and creating a List then assigning this all to the local var
-            return View(objCategoryList);
-        }
-
-        public IActionResult Create()
-        {
-            
-            return View();
-        }
-
-
+        _db = db;
     }
+
+    public IActionResult Index()
+    {
+        IEnumerable<Category> objCategoryList = _db.Categories; //added to retrieve all categories from _db database Categories table and creating a List then assigning this all to the local var
+        return View(objCategoryList);
+    }
+
+    //GET
+    public IActionResult Create()
+    {
+            
+        return View();
+    }
+
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Category obj)
+    {
+        if (obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name.");
+        }
+
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+
 }
